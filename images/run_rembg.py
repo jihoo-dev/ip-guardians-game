3dd41695009561344052b92387633cc7fb9a5d3b
jihoo-dev <won1640@gmail.com>
2026-08-25 14:38:17 +0900

    게임 사운드 관련 작업 및 배경스타일 적용

--- images/run_rembg.py
+++
@@ -1,9 +0,0 @@
-from rembg import remove
-
-with open("raw/run.png", "rb") as i:
-    with open("work/run.png", "wb") as o:
-        input_data = i.read()
-        output_data = remove(input_data)
-        o.write(output_data)
-
-print("배경 제거 완료!")
